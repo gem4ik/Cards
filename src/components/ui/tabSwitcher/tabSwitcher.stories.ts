a3f344fecbe5aae43319382cda8c1d849502c9e2
gem4ik <mgemskij@gmail.com>
2023-09-25 18:39:54 +0300

    minor fixes

--- src/components/ui/tabSwitcher/tabSwitcher.stories.ts
+++ src/components/ui/tabSwitcher/tabSwitcher.stories.ts
@@ -1,17 +1,18 @@
-import {Meta, StoryObj} from "@storybook/react";
-import {ToggleGroupDemo} from "@/components/ui/tabSwitcher/tabSwitcher.tsx";
+import { Meta, StoryObj } from '@storybook/react'
+
+import { ToggleGroupDemo } from '@/components/ui/tabSwitcher/tabSwitcher.tsx'
 
 const meta = {
-    title: 'Components/ToggleGroupDemo',
-    component: ToggleGroupDemo,
-    tags: ['autodocs'],
+  title: 'Components/ToggleGroupDemo',
+  component: ToggleGroupDemo,
+  tags: ['autodocs'],
 } satisfies Meta<typeof ToggleGroupDemo>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 export const Toggle: Story = {
-    args: {
-     values: ['hyeta','jopa']
-    },
+  args: {
+    values: ['hyeta', 'jopa'],
+  },
 }

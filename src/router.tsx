ab5b56a81eff6687434aae547f458b44631a99f6
gem4ik <mgemskij@gmail.com>
2023-10-01 10:18:34 +0300

    add store/base-api

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,40 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  Navigate,
+  Outlet,
+  RouteObject,
+  RouterProvider,
+} from 'react-router-dom'
 
-import { SignIn } from '@/components/auth/signIn/signIn.tsx'
+import { Layout } from '@/components/layout'
 
-const router = createBrowserRouter([
+const publicRoutes: RouteObject[] = [
+  {
+    path: '/login',
+    element: <Layout />,
+  },
+]
+
+const privateRoutes: RouteObject[] = [
   {
     path: '/',
-    element: <SignIn onSubmit={() => {}} />,
+    element: <Layout />,
   },
+]
+const router = createBrowserRouter([
+  {
+    element: <PrivateRoutes />,
+    children: privateRoutes,
+  },
+  ...publicRoutes,
 ])
 
 export const Router = () => {
   return <RouterProvider router={router} />
+}
+
+function PrivateRoutes() {
+  const isAuthenticated = false
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
 }

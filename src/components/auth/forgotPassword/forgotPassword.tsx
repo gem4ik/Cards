2068bfd667207919eff9e242f6b983f74ef85c30
gem4ik <mgemskij@gmail.com>
2023-10-02 21:15:32 +0300

    test

--- src/components/auth/forgotPassword/forgotPassword.tsx
+++ src/components/auth/forgotPassword/forgotPassword.tsx
@@ -1,68 +1,68 @@
-import s from './forgotPassword.module.scss'
-import {Header} from "@/components/ui/header";
-import {Card} from "@/components/ui/card/card.tsx";
-import {Typography} from "@/components/ui/typography";
-import {ControlledTextfield} from "@/components/ui/textfield/controlledTextfield.tsx";
-import {Button} from "@/components/ui/button";
-import {z} from "zod";
-import {useForm} from "react-hook-form";
-import {zodResolver} from "@hookform/resolvers/zod";
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
+import s from './forgotPassword.module.scss'
+
+import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card/card.tsx'
+import { Header } from '@/components/ui/header'
+import { ControlledTextfield } from '@/components/ui/textfield/controlledTextfield.tsx'
+import { Typography } from '@/components/ui/typography'
 
 type Data = {
-    email: string
+  email: string
 }
 type Props = {
-    onSubmit: (data: Data) => void
+  onSubmit: (data: Data) => void
 }
 const forgotPasswordSchema = z.object({
-    email: z.string().email()
+  email: z.string().email(),
 })
+
 export const ForgotPassword = (props: Props) => {
+  const { control, handleSubmit } = useForm<Data>({
+    mode: 'onSubmit',
+    defaultValues: {
+      email: '',
+    },
+    resolver: zodResolver(forgotPasswordSchema),
+  })
 
-    const {control, handleSubmit} = useForm<Data>({
-        mode: 'onSubmit',
-        defaultValues: {
-            email: '',
-        },
-        resolver: zodResolver(forgotPasswordSchema),
-    })
-    return (
-
-        <form
-            onSubmit={handleSubmit(data => {
-                props.onSubmit(data)
-                console.log(data)
-            })}
-        >
-            <div className={s.forgotPasswordWrapper}>
-                <Header isAuth={false}/>
-                <Card className={s.card}>
-                    <Typography style={{color: 'var(--color-light-100)'}} variant={'large'}>Forgot your password?
-                    </Typography>
-                    <div className={s.textFieldWrapper}>
-                        <ControlledTextfield control={control} name={'email'} label={'email'}/>
-                        <div className={s.textWrapper}>
-                            <Typography style={{color: 'var(--color-light-900)'}} variant={'body2'}>
-                                Enter your email address and we will send you further instructions
-                            </Typography>
-                        </div>
-                    </div>
-                    <div className={s.buttonWrapper}>
-                        <Button fullWidth={true}>Send Instructions</Button>
-                        <div className={s.buttonTextWrapper}>
-                            <Typography style={{color: 'var(--color-light-100)'}} variant={'body2'}>
-                                Did you remember your password?
-                            </Typography>
-                        </div>
-                        <div className={s.buttonLinkWrapper}>
-                            <Button variant={'link'}>Try logging in</Button>
-                        </div>
-                    </div>
-                </Card>
+  return (
+    <form
+      onSubmit={handleSubmit(data => {
+        props.onSubmit(data)
+        console.log(data)
+      })}
+    >
+      <div className={s.forgotPasswordWrapper}>
+        <Header isAuth={false} />
+        <Card className={s.card}>
+          <Typography style={{ color: 'var(--color-light-100)' }} variant={'large'}>
+            Forgot your password?
+          </Typography>
+          <div className={s.textFieldWrapper}>
+            <ControlledTextfield control={control} name={'email'} label={'email'} />
+            <div className={s.textWrapper}>
+              <Typography style={{ color: 'var(--color-light-900)' }} variant={'body2'}>
+                Enter your email address and we will send you further instructions
+              </Typography>
             </div>
-        </form>
-
-    );
-};
-
+          </div>
+          <div className={s.buttonWrapper}>
+            <Button fullWidth={true}>Send Instructions</Button>
+            <div className={s.buttonTextWrapper}>
+              <Typography style={{ color: 'var(--color-light-100)' }} variant={'body2'}>
+                Did you remember your password?
+              </Typography>
+            </div>
+            <div className={s.buttonLinkWrapper}>
+              <Button variant={'link'}>Try logging in</Button>
+            </div>
+          </div>
+        </Card>
+      </div>
+    </form>
+  )
+}

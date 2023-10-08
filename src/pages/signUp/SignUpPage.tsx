86fdfb3940df56ef7144607cfb357cda09dad453
KirillKozlo <kirillkozlogit@gmail.com>
2023-10-08 16:58:57 +0300

    Api SignUp add then and catch

--- src/pages/signUp/SignUpPage.tsx
+++ src/pages/signUp/SignUpPage.tsx
@@ -1,11 +1,15 @@
+import { useNavigate } from 'react-router-dom'
+
 import { SignUpForm } from '@/components/auth/signUp/signUp.tsx'
 import { useSignUpMutation } from '@/services/AuthAPI.ts'
 
 export const SignUpPage = () => {
   const [SignUp] = useSignUpMutation()
+  const navigate = useNavigate()
   const SingUpHandler = (email: string, password: string) => {
     SignUp({ email, password })
-    debugger
+      .then(() => navigate('login'))
+      .catch(error => console.log(error))
   }
 
   return <SignUpForm onSubmit={SingUpHandler} />

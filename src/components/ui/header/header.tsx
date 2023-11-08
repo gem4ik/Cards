bc5d982cc0e06238c20e95c731fe842f734fa323
gem4ik <mgemskij@gmail.com>
2023-11-08 23:49:23 +0300

    fix some styles

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -1,11 +1,13 @@
 import { ComponentPropsWithoutRef, forwardRef } from 'react'
 
+import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
 import s from './header.module.css'
 
 import { Logo } from '@/assets'
 import { Button, DropdownForHeader } from '@/components'
+import { appActions } from '@/services'
 
 export type UserProps = {
   photo?: string | null
@@ -22,10 +24,24 @@ type Props = {
 export const Header = forwardRef<HTMLHeadElement, Props>((props, ref) => {
   const { isAuth, user } = props
   const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const clearFilters = () => {
+    dispatch(appActions.setRangeValue(['0', '10']))
+    dispatch(appActions.setCurrentPage(1))
+    dispatch(appActions.setItemsPerPage('10'))
+    dispatch(appActions.setSearchName(''))
+    dispatch(appActions.setAuthor('All Cards'))
+  }
+
+  const loginClickHandler = () => {
+    navigate('/')
+    clearFilters()
+  }
 
   return (
     <header ref={ref} className={s.headerWrapper}>
-      <Logo onClick={() => navigate('/')} />
+      <Logo onClick={loginClickHandler} />
       {isAuth && <DropdownForHeader onLogOut={props.onLogOut} user={user} />}
       {!isAuth && (
         <Button onClick={() => navigate('/login')} type={'button'}>
